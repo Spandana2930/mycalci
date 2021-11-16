fa4cb4b6a399845e8c857ef48e3ce559df52841e
Spandana Morampudi <smorampudi@innominds.com>
2021-11-16 15:53:26 +0530

    html folder added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import './App.css';
 import FooterComponent from "./components/FooterComponent/footercomponent";
 import AboutPage from "./components/FooterComponent/Aboutpage/aboutpage";
 import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
-
+import BodyComponent from "./components/BodyComponent/bodycomponent"
+// import HeaderComponent from "./components/"
 function App() {
   return (
     <>
     <Router>
             <Switch>
-                <Route
-                    exact path="./components/FooterComponent/Aboutpage/aboutpag"
-                    render={()=><FooterComponent/>}
+            {/* <Route
+                    exact path="/"
+                    render={()=><HeaderComponent/>}
+                /> */}
+                
+            <Route
+                    exact path="/"
+                    render={()=><BodyComponent/>}
                 />
                 <Route
                     exact path="/"
+                    render={()=><FooterComponent/>}
+                />
+               
+                <Route
+                    exact path="/about"
                     render={()=><AboutPage/>}
                 />
         
---
+++ src/components/HeaderComponent/headercomponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Headercomponent extends Component {
+    render() {
+        return (
+            <>
+            <div class = "container d-flex flex-row align-items-center">
+           <h1>Calculator</h1>
+           </div>
+           </>
+        );
+    }
+}
+
+export default Headercomponent;
